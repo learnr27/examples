e7d544f2d07add31de755f2d9cfdfe46f9a79c00
cbb <learnr_27@126.com>
2020-05-26 17:04:23 +0800

    字段参数

--- django_orm/courses/models.py
+++ django_orm/courses/models.py
@@ -26,8 +26,8 @@ class Test(models.Model):
     Text = models.TextField(help_text='这个是longtext')  # longtext
 
     # 时间日期类型
-    Date = models.DateField(unique_for_date=True, auto_now=True)
-    Date_time = models.DateTimeField(editable=False, unique_for_month=True, auto_now_add=True)
+    Date = models.DateField(unique_for_date=True, auto_now=True)  # auto_now自动更新时间
+    Date_time = models.DateTimeField(editable=False, unique_for_month=True, auto_now_add=True)  # auto_now_add 自动添加插入时间
     Duration = models.DurationField()  # int, Python timedelta实现
 
     # 浮点型
@@ -50,7 +50,29 @@ class A(models.Model):
 
 class B(models.Model):
     foreign = models.ForeignKey(A)
+    foreign = models.ForeignKey(A, on_delete=models.CASCADE)  # 删除级联
+    foreign = models.ForeignKey(A, on_delete=models.PROTECT)
+    foreign = models.ForeignKey(A, on_delete=models.SET_NULL, null=True, blank=True)  # 删除置空
+    foreign = models.ForeignKey(A, on_delete=models.SET_DEFAULT, default=0)
+    foreign = models.ForeignKey(A, on_delete=models.DO_NOTHING)
+    foreign = models.ForeignKey(A, on_delete=models.SET)
 
 
 class C(models.Model):
     manytomany = models.ManyToManyField(B)
+
+
+# 1.所有字段都有的参数
+# 2.个别字段才有的参数
+# 3.关系型字段的参数
+
+"""
+on_delete 当一个被外键关联的对象被删除时，Django将模仿on_delete参数定义的SQL约束执行相应操作
+	如下6种操作
+	CASCADE：模拟SQL语言中的ON DELETE CASCADE约束，将定义有外键的模型对象同时删除！（该操作为当前Django版本的默认操作！）
+	PROTECT:阻止上面的删除操作，但是弹出ProtectedError异常
+	SET_NULL：将外键字段设为null，只有当字段设置了null=True时，方可使用该值。
+	SET_DEFAULT:将外键字段设为默认值。只有当字段设置了default参数时，方可使用。
+	DO_NOTHING：什么也不做。
+	SET()：设置为一个传递给SET()的值或者一个回调函数的返回值。注意大小写。
+"""
